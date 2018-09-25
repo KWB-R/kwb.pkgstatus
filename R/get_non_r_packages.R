4933b957fcb8b65b1b90590db2ed1dafc9111065
mrustl <michael.rustler@kompetenz-wasser.de>
2018-09-25 13:04:11 +0200

    add new private repo qrma.db to ignore

--- R/get_non_r_packages.R
+++ R/get_non_r_packages.R
@@ -8,5 +8,6 @@
 get_non_r_packages <- function() {
 
   c("kwb-r.github.io", "fakin.blog", "fakin.doc", 
-   "HydroServerLite", "GeoSalz", "FolderRights", "apps")
+   "HydroServerLite", "GeoSalz", "FolderRights", "apps",
+   "qmra.db")
 }
