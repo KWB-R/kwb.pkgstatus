1d57ace7b2f891e1dba93deea9fed54362348dce
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-05-25 22:07:29 +0200

    Update get_non_r_packages.R

--- R/get_non_r_packages.R
+++ R/get_non_r_packages.R
@@ -7,7 +7,7 @@
 #' 
 get_non_r_packages <- function() {
   
-  c("abimo", "apps", "abluft2", "basar.scripts", "kwb-r.github.io", "fakin", 
+  c("abimo", "apps", "abluft2", "basar.scripts", "dwc.scripts", "kwb-r.github.io", "fakin", 
     "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", "flusshygiene", 
     "HydroServerLite", "GeoSalz", "pathana", "pFromGrADS", "r-training", 
    "support", "maxflow", "pubs", "riverPollution", "smart.control", 
