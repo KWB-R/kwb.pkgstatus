405b674167a90a5ef2204e05df871cf823c2c293
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-06-01 10:54:49 +0200

    Add repo "dependencies" to non R packages

--- R/get_non_r_packages.R
+++ R/get_non_r_packages.R
@@ -7,8 +7,8 @@
 #' 
 get_non_r_packages <- function() {
   
-  c("abimo", "apps", "abluft2", "basar.scripts", "dwc.scripts", "kwb-r.github.io", "fakin", 
-    "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", "flusshygiene", 
+  c("abimo", "apps", "abluft2", "basar.scripts", "dependencies", "dwc.scripts", "kwb-r.github.io",  
+    "fakin", "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", "flusshygiene", 
     "HydroServerLite", "GeoSalz", "pathana", "pFromGrADS", "r-training", 
    "support", "maxflow", "pubs", "riverPollution", "smart.control", 
    "sema.scripts",	"spur", "status", "useR-2019", "wellma.scripts")
