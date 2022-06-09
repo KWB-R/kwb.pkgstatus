f828b1146beea3f1bd54350e653d800f504641c5
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2022-06-09 19:07:21 +0200

    Add new non R repos

--- R/get_non_r_packages.R
+++ R/get_non_r_packages.R
@@ -7,11 +7,11 @@
 #' 
 get_non_r_packages <- function() {
   
-  c("abimo", "apps", "abluft2", "basar.scripts", "dwc.scripts", "kwb-r.github.io", 
-    "fakin", "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", 
-    "flusshygiene", "HydroServerLite", "GeoSalz", "geosalz.mf", "geosalz.scripts", 
-    "pathana", "pFromGrADS", "qmra", "r-training", "support", "maxflow", "pubs", 
-    "riverPollution", "smart.control", "sema.scripts", "spur.scripts", "status", 
-    "universe", "useR-2019", "wellma.scripts")
+  c("abimo", "apps", "abluft2", "abluft2.scripts", "basar.scripts", "dwc.scripts", 
+    "kwb-r.github.io", "fakin", "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", 
+    "flusshygiene", "HydroServerLite", "hydrus1d", "GeoSalz", "geosalz.mf", "geosalz.scripts", 
+    "programming", "pathana", "pFromGrADS", "qmra", "r-training", "support", "maxflow",  
+    "misa.scripts", "pubs", "riverPollution", "smart.control", "sema.scripts", "spur.scripts",
+    "status", "universe", "useR-2019", "wellma.scripts")
 
 }
