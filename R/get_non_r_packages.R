96ef4135a22dba095f7591a91751430920bf702c
mrustl <michael.rustler@kompetenz-wasser.de>
2024-03-19 11:28:24 +0100

    Update get_non_r_packages.R

--- R/get_non_r_packages.R
+++ R/get_non_r_packages.R
@@ -7,11 +7,15 @@
 #' 
 get_non_r_packages <- function() {
   
-  c("abimo", "apps", "abluft2", "abluft2.scripts", "basar.scripts", "dwc.scripts", 
-    "kwb-r.github.io", "fakin", "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", 
-    "flusshygiene", "HydroServerLite", "hydrus1d", "GeoSalz", "geosalz.mf", "geosalz.scripts", 
-    "programming", "pathana", "pFromGrADS", "qmra", "r-training", "support", "maxflow",  
-    "mbr40.scripts", "misa.scripts", "pubs", "riverPollution", "smart.control", "sema.scripts", 
-    "spur.scripts", "status", "universe", "useR-2019", "wellma.scripts")
-
+  c("abimo", "abimo.scripts", "ad4gd_lakes", "apps", "abluft2", "abluft2.scripts", 
+    "basar.scripts", "dwc.scripts", "kwb-r.github.io", "kwb-r.r-universe.dev", 
+    "fakin", "fakin.blog", "fakin.doc", "fakin.scripts", "FolderRights", 
+    "flusshygiene", "HydroServerLite", "hydrus1d", "GeoSalz", "geosalz.mf", 
+    "geosalz.scripts", "impetus_scripts", "intruder.io", "lasso.scripts", 
+    "Logremoval", "programming", "pathana", "pFromGrADS", "promisces.hhra", 
+    "qmra", "qsimVis", "r-training", "support", "maxflow",  "mbr40.scripts", 
+    "misa.scripts", "pubs", "riverPollution", "smart.control", "sema.scripts", 
+    "sema.projects", "swim-ai", "spur.scripts", "status", "ultimate.scripts", 
+    "useR-2019", "wellma.scripts")
 }
+
