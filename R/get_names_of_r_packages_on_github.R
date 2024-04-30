6d56172c417c7afe1333b3bf90332f2b9fab8371
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-04-30 18:39:11 +0200

    Handle special case and error

--- R/get_names_of_r_packages_on_github.R
+++ R/get_names_of_r_packages_on_github.R
@@ -15,10 +15,10 @@ get_names_of_r_packages_on_github <- function(
     group = "kwb-r", public = TRUE, private = TRUE
 )
 {
-  # Names of repositories owned by group
+  # Get info on all repositories owned by group
   all_repos <- get_github_repos_impl(group = group)
   
-  # Repository type: private or public
+  # Is a repository private (or public)?
   is_private <- sapply(all_repos, `[[`, "private")
 
   # Keep only repositories of requested type
@@ -26,6 +26,11 @@ get_names_of_r_packages_on_github <- function(
   
   # Get the names of the selected repositories
   repo_names <- sapply(repos, `[[`, "name")
+  
+  # If there are no (remaining) repository names, return an empty vector
+  if (length(repos) == 0L) {
+    return(character(0L))
+  }
   
   # Keep only the names of repositories that look like R packages
   keep <- sapply(repo_names, github_repo_looks_like_r_package, owner = group)
@@ -35,6 +40,15 @@ get_names_of_r_packages_on_github <- function(
 # github_repo_looks_like_r_package ---------------------------------------------
 github_repo_looks_like_r_package <- function(owner = "kwb-r", repo)
 {
-  file_names <- list_files_in_github_repo(owner, repo)
+  file_names <- try(list_files_in_github_repo(owner, repo))
+
+  if (inherits(file_names, "try-error")) {
+    message(
+      "Error when trying to list files of repo '", repo, "'.\n",
+      "Returning FALSE (does not look like an R package)"
+    )
+    return(FALSE)
+  }
+  
   all(c("R/", "DESCRIPTION") %in% file_names)
 }
