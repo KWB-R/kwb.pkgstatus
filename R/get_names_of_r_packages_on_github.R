dd642710a6b162c19dfb817e5a3ca9f41f91a029
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-04-30 18:19:51 +0200

    Add get_names_of_r_packages_on_github()

---
+++ R/get_names_of_r_packages_on_github.R
@@ -0,0 +1,40 @@
+# get_names_of_r_packages_on_github --------------------------------------------
+
+#' Get Names of R Packages on GitHub
+#' 
+#' @param group name of organisation on GitHub, defaults to "kwb-r"
+#' @param public logical indicating whether private repositories are to be
+#'   considered. Default: \code{TRUE}
+#' @param private logical indicating whether public repositories are to be
+#'   considered. Default: \code{TRUE}
+#' @returns vector of character representing the names of the \code{public} 
+#'   and/or \code{private} repositories (as requested), owned by the 
+#'   organisation \code{groupt} on GitHub
+#' @export
+get_names_of_r_packages_on_github <- function(
+    group = "kwb-r", public = TRUE, private = TRUE
+)
+{
+  # Names of repositories owned by group
+  all_repos <- get_github_repos_impl(group = group)
+  
+  # Repository type: private or public
+  is_private <- sapply(all_repos, `[[`, "private")
+
+  # Keep only repositories of requested type
+  repos <- all_repos[(public & !is_private) | (private & is_private)]
+  
+  # Get the names of the selected repositories
+  repo_names <- sapply(repos, `[[`, "name")
+  
+  # Keep only the names of repositories that look like R packages
+  keep <- sapply(repo_names, github_repo_looks_like_r_package, owner = group)
+  repo_names[keep]
+}
+
+# github_repo_looks_like_r_package ---------------------------------------------
+github_repo_looks_like_r_package <- function(owner = "kwb-r", repo)
+{
+  file_names <- list_files_in_github_repo(owner, repo)
+  all(c("R/", "DESCRIPTION") %in% file_names)
+}
