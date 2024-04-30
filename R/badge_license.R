5dbf2cc044782ae6d77cc4753fe6aceeb37709df
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-04-30 06:39:17 +0200

    Clean badge_license.R
    - change order of commands in order to prolong
      the pipe
    - remove column renaming without effect
      (image_url -> badge_url)
    - rename result variable to "result"

--- R/badge_license.R
+++ R/badge_license.R
@@ -11,33 +11,29 @@
 #' @export
 badge_license <- function(license_keys, github_token = get_github_token()) 
 {
-  gh_licenses <- gh::gh(endpoint = "GET /licenses", .token =  github_token)
-  
-  gh_licenses_df <- data.table::rbindlist(gh_licenses, fill=TRUE)
-  
   #### License badges from: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
-  license_badges <- get_license_badge_info() %>% 
-    kwb.utils::renameColumns(list(image_url = "badge_url"))
+  license_badges <- get_license_badge_info()
   
-  gh_licenses_df <- dplyr::left_join(gh_licenses_df, license_badges) 
+  gh_licenses_df <- "GET /licenses" %>% 
+    gh::gh(.token = github_token) %>% 
+    data.table::rbindlist(fill = TRUE) %>% 
+    dplyr::left_join(license_badges) 
   
   gh_licenses_df$Badge_License <- image_link(
     image_name = gh_licenses_df$spdx_id, 
-    image_url = gh_licenses_df$badge_url, 
+    image_url = gh_licenses_df$image_url, 
     link_url = gh_licenses_df$license_url
   )
   
   badges <- gh_licenses_df %>%  
     dplyr::select_(~key, ~Badge_License) %>%  
-    dplyr::rename_(license_key = ~key)
+    dplyr::rename_(license_key = ~key) %>% 
+    dplyr::left_join(
+      x = data.frame(
+        license_key = license_keys, 
+        stringsAsFactors = FALSE
+      )
+    )
   
-  result <- dplyr::left_join(
-    x = data.frame(
-      license_key = license_keys, 
-      stringsAsFactors = FALSE
-    ), 
-    y = badges
-  )
-  
-  result$Badge_License
+  badges$Badge_License
 }
